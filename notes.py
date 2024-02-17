c948d364fb98a257702f555de62babef869a6dc3
Igor <ygarik@mail.ru>
2024-02-17 16:42:36 +0300

    добавил удаление, добавил выбор по ID, если поиск дал  несколько результатов

--- notes.py
+++ notes.py
@@ -27,41 +27,63 @@ def show(file_name: str):
             print(*item)
     return lst1
 
-def find (file_name: str):
 
-        show(file_name)
-        
-        attr = input("Введите заголовок для поиска: ")
-        with open(file_name, "r", encoding="utf-8") as f:
-            data = f.readlines()
-            data = list(filter(lambda x: x.split(", ")[1] == attr, data))
-            x = list(zip(range(1,len(data)+1), data))
-            for item in x:
-                print(*item)
-        #     if (data == []):
-        #         print("Заметки с таким заголовком не найдено")
-        #     else:
-        #         print(data)
-           
-        return data[0]
+def find(file_name: str, option:bool):
 
-def cover(file_name: str):
-    old_data = find(file_name)
-    old_id = old_data[0]
-    print(old_data)
-    print("Введите новые данные")
-    new_title = input("Введите новый заголовок: ")
-    new_note_text = input("Введите новый текст заметки: ")
-    time = datetime.datetime.now()
+    show(file_name)
+
+    attr = input("Введите заголовок для поиска: ")
+    print()
     with open(file_name, "r", encoding="utf-8") as f:
         data = f.readlines()
-       
-        i = data.index(old_data)
-       
-        data[i] = f"{old_id}, {new_title}, {new_note_text}, {time}\n"
-    with open(file_name, "w", encoding="utf-8") as f:
-        f.writelines(data)
-    show(file_name)
+        data = list(filter(lambda x: x.split(", ")[1] == attr, data))
+        x = list(zip(range(1, len(data)+1), data))
+        for item in x:
+            print(*item)
+        if (data != []):
+            if option:
+                id = input("Введите ID нужной заметки: ")
+                result = list(filter(lambda x: x.split(", ")[0] == id, data))
+                return result[0]
+                
+
+        else:
+            print("Заметки с таким заголовком не найдено")
+            print()
+
+
+def cover(file_name: str):
+    
+    old_data = find(file_name, True)
+    if(old_data != None):
+        old_id = old_data.split(", ")[0]
+        print("Введите новые данные")
+        new_title = input("Введите новый заголовок: ")
+        new_note_text = input("Введите новый текст заметки: ")
+        time = datetime.datetime.now()
+        with open(file_name, "r", encoding="utf-8") as f:
+            data = f.readlines()
+
+            i = data.index(old_data)
+
+            data[i] = f"{old_id}, {new_title}, {new_note_text}, {time}\n"
+        with open(file_name, "w", encoding="utf-8") as f:
+            f.writelines(data)
+        show(file_name)
+
+
+def delete(file_name: str):
+    deleting_data = find(file_name, True)
+    if (deleting_data != None):
+        
+        with open(file_name, "r", encoding="utf-8") as f:
+            data = f.readlines()
+            s = deleting_data
+            data.remove(s)
+        with open(file_name, "w", encoding="utf-8") as f:
+            f.writelines(data)
+        show(file_name)
+
 
 def main():
     file_name = 'notebook.txt'
@@ -71,15 +93,18 @@ def main():
         print("2 - добавить новую запись")
         print("3 - искать по заголовку")
         print("4 - корректировать заметку")
+        print("5 - удалить заметку")
         answer = input("Введите номер команды или 'x' для выхода:  ")
         if answer == "1":
             show(file_name)
         elif answer == "2":
             add(file_name)
-        elif answer =="3":
-            find(file_name)
-        elif answer =="4":
+        elif answer == "3":
+            find(file_name, False)
+        elif answer == "4":
             cover(file_name)
+        elif answer == "5":
+            delete(file_name)
         elif answer == "x":
             flag_exit = True
 
